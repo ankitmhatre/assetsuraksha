c34fc8e68114fe700f36797d1c361453521eed2c
rtrend96 <rtrend96@gmail.com>
2019-03-29 03:38:44 +0530

    form added

--- src/Components/UserProfile/MainContent.js
+++ src/Components/UserProfile/MainContent.js
@@ -2,59 +2,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import NSCmain from '../Assets/NSC/NSCmain/NSCmain';
-import KVP from "../Assets/KVP/KVP";
+import KVPmain from '../Assets/KVP/KVPmain/KVPmain';
 import MF from "../Assets/MF/MF";
 import PhysicalShare from "../Assets/PhysicalShare/PhysicalShare";
 import RealEstate from "../Assets/RealEstate/RealEstate";
 import DP_Stock from "../Assets/DP_Stock/DP_Stock";
 import PDetails from '../Forms/PDetails/PDetails';
-import NSCform from '../Assets/NSC/NSCform/NSCform';
-
+import KVPForm from '../Assets/KVP/KVPform/KVPform';
+import NSCForm from '../Assets/NSC/NSCform/NSCform';
 const drawerWidth = 240;
 
 const styles = theme => ({
-root: {
-	display: 'flex',
-},
-toolbar: theme.mixins.toolbar,
-content: {
-	flexGrow: 1,
-	padding: theme.spacing.unit * 3,
-},
+	root: {
+		display: 'flex',
+	},
+	toolbar: theme.mixins.toolbar,
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing.unit * 3,
+	},
 });
 
 class MainContent extends React.Component {
 
-render() {
-	const { classes, theme } = this.props;
+	render() {
+		const { classes, theme } = this.props;
 
-	return (
-		<div className={classes.root}>
-			<main className={classes.content}>
-				<div className={classes.toolbar} />
+		return (
+			<div className={classes.root}>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
 
-				{this.props.displayNSCForm && <NSCform />}
+					{this.props.displayNSCForm && <NSCForm />}
+					{this.props.displayKVPForm && <KVPForm />}
 
-				{this.props.pDetails && <PDetails style={{marginLeft: '30px'}}/>}
-				{this.props.NSC && <NSCmain DisplayNSCForm={this.props.DisplayNSCForm} />}
-				{this.props.KVP && <KVP />}
-				{this.props.MF && <MF />}
-				{this.props.physicalShare && <PhysicalShare />}
-				{this.props.realEstate && <RealEstate />}
-				{this.props.dp_stock && <DP_Stock />}
-				
-			</main>
-		</div>
-	);
-}
+					{this.props.pDetails && <PDetails style={{ marginLeft: '30px' }} />}
+					{this.props.NSC && <NSCmain DisplayNSCForm={this.props.DisplayNSCForm} />}
+					{this.props.KVP && <KVPmain DisplayKVPForm={this.props.DisplayKVPForm} />}
+					{this.props.MF && <MF />}
+					{this.props.physicalShare && <PhysicalShare />}
+					{this.props.realEstate && <RealEstate />}
+					{this.props.dp_stock && <DP_Stock />}
+
+				</main>
+			</div>
+		);
+	}
 }
 
 MainContent.propTypes = {
-classes: PropTypes.object.isRequired,
-// Injected by the documentation to work in an iframe.
-// You won't need it on your project.
-container: PropTypes.object,
-theme: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
+	// Injected by the documentation to work in an iframe.
+	// You won't need it on your project.
+	container: PropTypes.object,
+	theme: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles, { withTheme: true })(MainContent);
--- src/Containers/UserProfile/UserProfile.js
+++ src/Containers/UserProfile/UserProfile.js
@@ -10,204 +10,223 @@ import MainContent from '../../Components/UserProfile/MainContent';
 const drawerWidth = 240;
 
 const styles = theme => ({
-root: {
-	display: 'flex',
-},
-drawer: {
-	[theme.breakpoints.up('sm')]: {
+	root: {
+		display: 'flex',
+	},
+	drawer: {
+		[theme.breakpoints.up('sm')]: {
+			width: drawerWidth,
+			flexShrink: 0,
+		},
+	},
+	appBar: {
+		marginLeft: drawerWidth,
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+		},
+	},
+	menuButton: {
+		marginRight: 20,
+		[theme.breakpoints.up('sm')]: {
+			display: 'none',
+		},
+	},
+	toolbar: theme.mixins.toolbar,
+	drawerPaper: {
 		width: drawerWidth,
-		flexShrink: 0,
 	},
-},
-appBar: {
-	marginLeft: drawerWidth,
-	[theme.breakpoints.up('sm')]: {
-		width: `calc(100% - ${drawerWidth}px)`,
+	nested: {
+		paddingLeft: theme.spacing.unit * 4,
 	},
-},
-menuButton: {
-	marginRight: 20,
-	[theme.breakpoints.up('sm')]: {
-		display: 'none',
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing.unit * 3,
 	},
-},
-toolbar: theme.mixins.toolbar,
-drawerPaper: {
-	width: drawerWidth,
-},
-nested: {
-	paddingLeft: theme.spacing.unit * 4,
-},
-content: {
-	flexGrow: 1,
-	padding: theme.spacing.unit * 3,
-},
 });
 
 class UserProfile extends React.Component {
-state = {
-	mobileOpen: false,
-	pDetails: false,
-	assets: false,
-	KVP: false,
-	NSC: false,
-	physicalShare: false,
-	MF: false,
-	realEstate: false,
-	dp_stock: false,
-	displayNSCForm: false
-};
-
-handleAssets = () => {
-	this.setState(state => ({ assets: !state.assets }));
-};
-
-handleDrawerToggle = () => {
-	this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-};
-
-displayPDetails = () => {
-	this.setState({
+	state = {
+		mobileOpen: false,
+		pDetails: false,
+		assets: false,
 		KVP: false,
-		pDetails: true,
 		NSC: false,
 		physicalShare: false,
 		MF: false,
 		realEstate: false,
 		dp_stock: false,
 		displayNSCForm: false,
-	})
-}
+		displayKVPform: false
+	};
 
-displayKVP = () => {
-	this.setState({
-		pDetails: false,
-		KVP: true,
-		NSC: false,
-		physicalShare: false,
-		MF: false,
-		realEstate: false,
-		dp_stock: false,
-		displayNSCForm: false,
-	})
-}
+	handleAssets = () => {
+		this.setState(state => ({ assets: !state.assets }));
+	};
 
-displayNSC = () => {
-	this.setState({
-		pDetails: false,
-		KVP: false,
-		NSC: true,
-		physicalShare: false,
-		MF: false,
-		realEstate: false,
-		dp_stock: false,
-		displayNSCForm: false,
-	})
-}
+	handleDrawerToggle = () => {
+		this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+	};
 
-displayMP = () => {
-	this.setState({
-		pDetails: false,
-		KVP: false,
-		NSC: false,
-		physicalShare: false,
-		MF: true,
-		realEstate: false,
-		dp_stock: false,
-		displayNSCForm: false,
-	})
-}
+	displayPDetails = () => {
+		this.setState({
+			KVP: false,
+			pDetails: true,
+			NSC: false,
+			physicalShare: false,
+			MF: false,
+			realEstate: false,
+			dp_stock: false,
+			displayKVPForm: false,
+			displayNSCForm: false,
+		})
+	}
 
-displayRealEstate = () => {
-	this.setState({
-		pDetails: false,
-		KVP: false,
-		NSC: false,
-		physicalShare: false,
-		MF: false,
-		realEstate: true,
-		dp_stock: false,
-		displayNSCForm: false,
-	})
-}
+	displayKVP = () => {
+		this.setState({
+			pDetails: false,
+			KVP: true,
+			NSC: false,
+			physicalShare: false,
+			MF: false,
+			realEstate: false,
+			dp_stock: false,
+			displayKVPForm: false,
+			displayNSCForm: false,
+		})
+	}
 
-displayPhysicalShare = () => {
-	this.setState({
-		pDetails: false,
-		KVP: false,
-		NSC: false,
-		physicalShare: true,
-		MF: false,
-		realEstate: false,
-		dp_stock: false,
-		displayNSCForm: false,
-	})
-}
+	displayNSC = () => {
+		this.setState({
+			pDetails: false,
+			KVP: false,
+			NSC: true,
+			physicalShare: false,
+			MF: false,
+			realEstate: false,
+			dp_stock: false,
+			displayNSCForm: false,
+			displayKVPForm: false,
+		})
+	}
 
-displayDP_Stock = () => {
-	this.setState({
-		pDetails: false,
-		KVP: false,
-		NSC: false,
-		physicalShare: false,
-		MF: false,
-		realEstate: false,
-		dp_stock: true,
-		displayNSCForm: false,
-	})
-}
+	displayMP = () => {
+		this.setState({
+			pDetails: false,
+			KVP: false,
+			NSC: false,
+			physicalShare: false,
+			MF: true,
+			realEstate: false,
+			dp_stock: false,
+			displayNSCForm: false,
+			displayKVPForm: false,
+		})
+	}
 
-DisplayNSCForm = () => {
-	this.setState({
-		NSC: false,
-		displayNSCForm: true,
-	})
-}	
+	displayRealEstate = () => {
+		this.setState({
+			pDetails: false,
+			KVP: false,
+			NSC: false,
+			physicalShare: false,
+			MF: false,
+			realEstate: true,
+			dp_stock: false,
+			displayKVPForm: false,
+			displayNSCForm: false,
+		})
+	}
 
-render() {
-	const { classes, theme } = this.props;
+	displayPhysicalShare = () => {
+		this.setState({
+			pDetails: false,
+			KVP: false,
+			NSC: false,
+			physicalShare: true,
+			MF: false,
+			realEstate: false,
+			dp_stock: false,
+			displayNSCForm: false,
+			displayKVPForm: false,
+		})
+	}
 
-	return (
-		<div className={classes.root}>
-		<CssBaseline />
-		
-		<NavBar handleDrawerToggle={this.handleDrawerToggle}/>
+	displayDP_Stock = () => {
+		this.setState({
+			pDetails: false,
+			KVP: false,
+			NSC: false,
+			physicalShare: false,
+			MF: false,
+			realEstate: false,
+			dp_stock: true,
+			displayNSCForm: false,
+			displayKVPForm: false,
+		})
+	}
 
-		<UserProfileDrawer
-			mobileOpen={this.state.mobileOpen}
-			handleDrawerToggle={this.handleDrawerToggle}
-			handleAssets={this.handleAssets}
-			assets={this.state.assets}
-			displayKVP={this.displayKVP}
-			displayNSC={this.displayNSC}
-			displayMP={this.displayMP}
-			displayRealEstate={this.displayRealEstate}
-			displayPhysicalShare={this.displayPhysicalShare}
-			displayDP_Stock={this.displayDP_Stock}
-			displayPDetails={this.displayPDetails}
-			/>
+	DisplayNSCForm = () => {
+		this.setState({
+			NSC: false,
+			displayNSCForm: true,
+		})
+	}
 
-		<MainContent
-			KVP={this.state.KVP}
-			NSC={this.state.NSC}
-			physicalShare={this.state.physicalShare}
-			MF={this.state.MF}
-			realEstate={this.state.realEstate}
-			dp_stock={this.state.dp_stock}
-			pDetails={this.state.pDetails}
-			DisplayNSCForm={this.DisplayNSCForm}
-			displayNSCForm={this.state.displayNSCForm} />
-		</div>
-	);
-}
+	DisplayKVPForm = () => {
+		this.setState({
+			KVP: false,
+			displayKVPForm: true,
+		})
+	}
+
+
+
+	render() {
+		const { classes, theme } = this.props;
+
+		return (
+			<div className={classes.root}>
+				<CssBaseline />
+
+				<NavBar handleDrawerToggle={this.handleDrawerToggle} />
+
+				<UserProfileDrawer
+					mobileOpen={this.state.mobileOpen}
+					handleDrawerToggle={this.handleDrawerToggle}
+					handleAssets={this.handleAssets}
+					assets={this.state.assets}
+					displayKVP={this.displayKVP}
+					displayNSC={this.displayNSC}
+					displayMP={this.displayMP}
+					displayRealEstate={this.displayRealEstate}
+					displayPhysicalShare={this.displayPhysicalShare}
+					displayDP_Stock={this.displayDP_Stock}
+					displayPDetails={this.displayPDetails}
+				/>
+
+				<MainContent
+					KVP={this.state.KVP}
+					NSC={this.state.NSC}
+					physicalShare={this.state.physicalShare}
+					MF={this.state.MF}
+					realEstate={this.state.realEstate}
+					dp_stock={this.state.dp_stock}
+					pDetails={this.state.pDetails}
+					DisplayKVPForm={this.DisplayKVPForm}
+					displayKVPForm={this.state.displayKVPForm}
+					DisplayNSCForm={this.DisplayNSCForm}
+					displayNSCForm={this.state.displayNSCForm} />
+			</div>
+		);
+	}
 }
 
 UserProfile.propTypes = {
-classes: PropTypes.object.isRequired,
-// Injected by the documentation to work in an iframe.
-// You won't need it on your project.
-container: PropTypes.object,
-theme: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
+	// Injected by the documentation to work in an iframe.
+	// You won't need it on your project.
+	container: PropTypes.object,
+	theme: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles, { withTheme: true })(UserProfile);
