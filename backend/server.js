51719a5b0c7ed5d3ef35988d1d1af50666f9f2b2
Ankit <ankit@1337lab.io>
2019-05-04 18:41:11 +0530

    User Schema Object added

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,40 @@
-const http = require('http');
 const mongoose = require('mongoose');
-const hostname = '127.0.0.1';
-const port = 3000;
-
 var cors = require('cors')
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
-var url = require('url');
-
-
+var url = require('url')
 var bodyParser = require('body-parser')
+
+//Mongoose
+mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true});
+var db = mongoose.connection;
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+app.use(bodyParser.urlencoded({extended: true}));   // to support URL-encoded bodies
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 app.use(cors())
 
 
 
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("AssetSuraksha Server listening at http://%s:%s", host, port)
+
+  
+   db.on('error', console.error.bind(console, 'connection error:'));
+   db.once('open', function() {
+    console.log("We are connected to mongo database");
+   });
+})
+
 
 
 
 app.post('/checkUserExists', function (req, res) {
-   // First read existing users.
-
+   // check from db  
    var ans = {}
    ans["username"] = req.body.username
    ans["doesExists"]= true
@@ -35,17 +42,3 @@ app.post('/checkUserExists', function (req, res) {
    res.end( JSON.stringify(ans));
 })
 
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-
-   mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true});
-   var db = mongoose.connection;
-   db.on('error', console.error.bind(console, 'connection error:'));
-   db.once('open', function() {
-    console.log("We are connected to Monogo database");
-   });
-   
-
-})
