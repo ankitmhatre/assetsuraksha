f28e0fe1b4a40b4c8237050241c26d20c5bcde01
Ankit <ankit@1337lab.io>
2019-05-06 08:07:37 +0530

    Tokenization sorted
    send {email, password} in body  to localhost:3000/users/signup
    and hen  to localhost:3000/users/login

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,8 @@
-const mongoose = require('mongoose');
-var cors = require('cors')
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var url = require('url')
-var bodyParser = require('body-parser')
+const http = require('http');
+const app = require('./app');
 
-//Mongoose
-mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true});
-var db = mongoose.connection;
+const port = process.env.PORT || 3000;
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({extended: true}));   // to support URL-encoded bodies
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
-app.use(cors())
+const server = http.createServer(app);
 
-
-
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("AssetSuraksha Server listening at http://%s:%s", host, port)
-
-  
-   db.on('error', console.error.bind(console, 'connection error:'));
-   db.once('open', function() {
-    console.log("We are connected to mongo database");
-   });
-})
-
-
-
-
-app.post('/checkUserExists', function (req, res) {
-   // check from db  
-   var ans = {}
-   ans["username"] = req.body.username
-   ans["doesExists"]= true
-   
-   res.end( JSON.stringify(ans));
-})
-
+server.listen(port);
